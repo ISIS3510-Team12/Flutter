21ba2832ed37319426519932cd335c89867a0eb9
valiwisdev <v.caro@uniandes.edu.co>
2026-09-12 21:06:10 -0500

    feat: rename MainScreen and CustomNavigationBar constructors; add AppColorScheme, AppTheme, and AppFontWeight classes

---
+++ lib/ui/core/themes/app_theme.dart
@@ -0,0 +1,68 @@
+import "package:flutter/material.dart";
+import "package:team12_flutter_juggle/ui/core/themes/app_color_scheme.dart";
+
+class AppTheme {
+
+  final TextTheme textTheme;
+  
+  const AppTheme(this.textTheme);
+
+  ThemeData light() {
+    return theme(AppColorScheme.lightScheme);
+  }
+
+  ThemeData dark() {
+    return theme(AppColorScheme.darkScheme);
+  }
+
+  ThemeData theme(ColorScheme colorScheme) => ThemeData(
+     useMaterial3: true,
+     brightness: colorScheme.brightness,
+     colorScheme: colorScheme,
+     textTheme: textTheme.apply(
+       bodyColor: colorScheme.onSurface,
+       displayColor: colorScheme.onSurface,
+     ),
+     scaffoldBackgroundColor: colorScheme.surface,
+     canvasColor: colorScheme.surface,
+  );
+
+
+  List<ExtendedColor> get extendedColors => [
+  ];
+}
+
+class ExtendedColor {
+  final Color seed, value;
+  final ColorFamily light;
+  final ColorFamily lightHighContrast;
+  final ColorFamily lightMediumContrast;
+  final ColorFamily dark;
+  final ColorFamily darkHighContrast;
+  final ColorFamily darkMediumContrast;
+
+  const ExtendedColor({
+    required this.seed,
+    required this.value,
+    required this.light,
+    required this.lightHighContrast,
+    required this.lightMediumContrast,
+    required this.dark,
+    required this.darkHighContrast,
+    required this.darkMediumContrast,
+  });
+}
+
+class ColorFamily {
+  const ColorFamily({
+    required this.color,
+    required this.onColor,
+    required this.colorContainer,
+    required this.onColorContainer,
+  });
+
+  final Color color;
+  final Color onColor;
+  final Color colorContainer;
+  final Color onColorContainer;
+}
